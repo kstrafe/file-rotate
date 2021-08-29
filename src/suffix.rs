2d638e5b1db942333085fe0c67bc151ff5a5443b
Erlend Langseth <3rlendhl@gmail.com>
2021-08-29 14:38:21 +0200

    SuffixScheme implementations are lighter, more logic in FileRotate
    Now the SuffixScheme functions don't do file operations. FileRotate
    takes care of cascading files if necessary, removing files, keeping
    track of log paths etc.

--- src/suffix.rs
+++ src/suffix.rs
@@ -1,21 +1,47 @@
-use chrono::NaiveDateTime;
 #[cfg(feature = "chrono04")]
-use chrono::{offset::Local, Duration};
+use chrono::{offset::Local, Duration, NaiveDateTime};
 use std::{
-    collections::VecDeque,
+    cmp::Ordering,
     path::{Path, PathBuf},
 };
 
+/// Representation of a suffix
+/// `Ord + PartialOrd`: sort by age of the suffix. Most recent first (smallest).
+pub trait Representation: Ord + ToString + Eq + Clone + std::fmt::Debug {
+    /// Create path
+    fn to_path(&self, basepath: &Path) -> PathBuf {
+        PathBuf::from(format!("{}.{}", basepath.display(), self.to_string()))
+    }
+}
+
 /// How to move files: How to rename, when to delete.
 pub trait SuffixScheme {
-    /// Returns new suffix to which to move current log file (does not do the move).
-    /// Deletes old log files.
-    /// Might also do other operations, like moving files in a cascading way.
-    fn rotate(&mut self, basepath: &Path) -> String;
+    /// The representation of suffixes that this suffix scheme uses.
+    /// E.g. if the suffix is a number, you can use `usize`.
+    type Repr: Representation;
 
-    /// Get paths of rotated log files, in order from newest to oldest.
-    /// Excludes the suffix-less log file.
-    fn log_paths(&mut self, basepath: &Path) -> Vec<PathBuf>;
+    /// The file at `suffix` needs to be rotated.
+    /// Returns the target file path.
+    /// The file will be moved outside this function.
+    /// If the target path already exists, rotate_file is called again with `path` set to the
+    /// target path.  Thus it cascades files by default, and if this is not desired, it's up to
+    /// `rotate_file` to return a path that does not already exist.
+    ///
+    /// `prev_suffix` is provided just in case it's useful (not always)
+    fn rotate_file(
+        &mut self,
+        basepath: &Path,
+        newest_suffix: Option<&Self::Repr>,
+        suffix: &Option<Self::Repr>,
+    ) -> Self::Repr;
+
+    /// Parse suffix from string.
+    fn parse(&self, suffix: &str) -> Option<Self::Repr>;
+
+    /// Whether either the suffix or the chronological file number indicates that the file is old
+    /// and should be deleted, depending of course on the file limit.
+    /// `file_number` starts at 0.
+    fn too_old(&self, suffix: &Self::Repr, file_number: usize) -> bool;
 }
 
 /// Rotated log files get a number as suffix. The greater the number, the older. The oldest files
@@ -31,63 +57,23 @@ impl CountSuffix {
     }
 }
 
+impl Representation for usize {}
 impl SuffixScheme for CountSuffix {
-    fn rotate(&mut self, basepath: &Path) -> String {
-        /// Make sure that path(count) does not exist, by moving it to path(count+1).
-        fn cascade(basepath: &Path, count: usize, max_files: usize) {
-            let src = PathBuf::from(format!("{}.{}", basepath.display(), count));
-            if src.exists() {
-                let dest = PathBuf::from(format!("{}.{}", basepath.display(), count + 1));
-                if dest.exists() {
-                    cascade(basepath, count + 1, max_files);
-                }
-                if count >= max_files {
-                    // If the file is too old (too big count), delete it,
-                    //   (also if count == max_files, because then the .(max_files-1) file will be moved
-                    //   to .max_files)
-                    let _ = std::fs::remove_file(&src).unwrap();
-                } else {
-                    // otherwise, rename it.
-                    let _ = std::fs::rename(src, dest);
-                }
-            }
+    type Repr = usize;
+    fn rotate_file(&mut self,
+        _basepath: &Path,
+        _: Option<&usize>,
+        suffix: &Option<usize>) -> usize {
+        match suffix {
+            Some(suffix) => suffix + 1,
+            None => 1,
         }
-        cascade(basepath, 1, self.max_files);
-        "1".to_string()
     }
-    fn log_paths(&mut self, basepath: &Path) -> Vec<PathBuf> {
-        let filename_prefix = &*basepath
-            .file_name()
-            .expect("basepath.file_name()")
-            .to_string_lossy();
-        let filenames = std::fs::read_dir(basepath.parent().expect("basepath.parent()"))
-            .unwrap()
-            .filter_map(|entry| entry.ok())
-            .filter(|entry| entry.path().is_file())
-            .map(|entry| entry.file_name());
-        let mut numbers = Vec::new();
-        for filename in filenames {
-            let filename = filename.to_string_lossy();
-            if !filename.starts_with(&filename_prefix) {
-                continue;
-            }
-            if let Some(dot) = filename.find('.') {
-                let suffix = &filename[(dot + 1)..];
-                if let Ok(n) = suffix.parse::<usize>() {
-                    numbers.push(n);
-                } else {
-                    continue;
-                }
-            } else {
-                // We don't consider the current (suffix-less) log file.
-            }
-        }
-        // Sort descending - the largest numbers are the oldest and thus should come first
-        numbers.sort_by(|x, y| y.cmp(x));
-        numbers
-            .iter()
-            .map(|n| PathBuf::from(format!("{}.{}", basepath.display(), n)))
-            .collect::<Vec<_>>()
+    fn parse(&self, suffix: &str) -> Option<usize> {
+        suffix.parse::<usize>().ok()
+    }
+    fn too_old(&self, _suffix: &usize, file_number: usize) -> bool {
+        file_number >= self.max_files
     }
 }
 
@@ -95,30 +81,23 @@ impl SuffixScheme for CountSuffix {
 ///  - Neither `format` or the base filename can include the character `"."`.
 ///  - The `format` should ensure that the lexical and chronological orderings are the same
 #[cfg(feature = "chrono04")]
-pub struct TimestampSuffix {
-    /// None means that we don't know the files, and a scan is necessary.
-    pub(crate) suffixes: Option<VecDeque<(String, Option<usize>)>>,
+pub struct TimestampSuffixScheme {
     format: &'static str,
     file_limit: FileLimit,
 }
 
 #[cfg(feature = "chrono04")]
-impl TimestampSuffix {
+impl TimestampSuffixScheme {
     /// With format `"%Y%m%dT%H%M%S"`
     pub fn default(file_limit: FileLimit) -> Self {
         Self {
-            suffixes: None,
             format: "%Y%m%dT%H%M%S",
             file_limit,
         }
     }
-    /// Create new TimestampSuffix suffix scheme
+    /// Create new TimestampSuffixScheme suffix scheme
     pub fn with_format(format: &'static str, file_limit: FileLimit) -> Self {
-        Self {
-            suffixes: None,
-            format,
-            file_limit,
-        }
+        Self { format, file_limit }
     }
     /// NOTE: For future use in RotationMode::Custom
     pub fn should_rotate(&self, age: Duration) -> impl Fn(&str) -> bool {
@@ -128,148 +107,130 @@ impl TimestampSuffix {
             suffix < old_timestamp.as_str()
         }
     }
-    pub(crate) fn suffix_to_string(&self, suffix: &(String, Option<usize>)) -> String {
-        match suffix.1 {
-            Some(n) => format!("{}.{}", suffix.0, n),
-            None => suffix.0.clone(),
-        }
-    }
-    pub(crate) fn suffix_to_path(
-        &self,
-        basepath: &Path,
-        suffix: &(String, Option<usize>),
-    ) -> PathBuf {
-        PathBuf::from(format!(
-            "{}.{}",
-            basepath.display(),
-            self.suffix_to_string(suffix)
-        ))
-    }
-    /// Scan files in the log directory to construct the list of files
-    fn ensure_suffix_list(&mut self, basepath: &Path) {
-        if self.suffixes.is_none() {
-            let mut suffixes = VecDeque::new();
-            let filename_prefix = &*basepath
-                .file_name()
-                .expect("basepath.file_name()")
-                .to_string_lossy();
-            let parent = basepath.parent().unwrap();
-            let filenames = std::fs::read_dir(parent)
-                .unwrap()
-                .filter_map(|entry| entry.ok())
-                .filter(|entry| entry.path().is_file())
-                .map(|entry| entry.file_name());
-            for filename in filenames {
-                let filename = filename.to_string_lossy();
-                if !filename.starts_with(&filename_prefix) {
-                    continue;
-                }
-                // Find the up to two `.` in the filename
-                if let Some(first_dot) = filename.find('.') {
-                    let suffix = &filename[(first_dot + 1)..];
-                    let (timestamp_str, n) = if let Some(second_dot) = suffix.find('.') {
-                        if let Ok(n) = suffix[(second_dot + 1)..].parse::<usize>() {
-                            (&suffix[..second_dot], Some(n))
-                        } else {
-                            continue;
-                        }
-                    } else {
-                        (suffix, None)
-                    };
-                    if NaiveDateTime::parse_from_str(timestamp_str, self.format).is_ok() {
-                        suffixes.push_back((timestamp_str.to_string(), n))
-                    }
-                } else {
-                    // We don't consider the current (suffix-less) log file.
-                }
-            }
-            // Sort in Ascending order (higher value (most recent) first)
-            suffixes
-                .make_contiguous()
-                .sort_by_key(|suffix| self.suffix_to_string(suffix));
-            self.suffixes = Some(suffixes);
+}
+
+/// Structured representation of the suffixes of TimestampSuffixScheme.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct TimestampSuffix {
+    timestamp: String,
+    number: Option<usize>,
+}
+impl Representation for TimestampSuffix {}
+impl Ord for TimestampSuffix {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Most recent = smallest (opposite as the timestamp Ord)
+        // Smallest = most recent. Thus, biggest timestamp first. And then biggest number
+        match other.timestamp.cmp(&self.timestamp) {
+            Ordering::Equal => other.number.cmp(&self.number),
+            unequal => unequal,
         }
     }
 }
+impl PartialOrd for TimestampSuffix {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl std::fmt::Display for TimestampSuffix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self.number {
+            Some(n) => write!(f, "{}.{}", self.timestamp, n),
+            None => write!(f, "{}", self.timestamp),
+        }
+    }
+}
+
 #[cfg(feature = "chrono04")]
-impl SuffixScheme for TimestampSuffix {
-    fn rotate(&mut self, basepath: &Path) -> String {
-        let now = Local::now().format(self.format).to_string();
+impl SuffixScheme for TimestampSuffixScheme {
+    type Repr = TimestampSuffix;
 
-        self.ensure_suffix_list(basepath);
+    fn rotate_file(
+        &mut self,
+        _basepath: &Path,
+        newest_suffix: Option<&TimestampSuffix>,
+        suffix: &Option<TimestampSuffix>,
+    ) -> TimestampSuffix {
+        if suffix.is_none() {
+            let now = Local::now().format(self.format).to_string();
 
-        // For all existing suffixes that equals `now`, take the max `n`, and add one
-        let n = self
-            .suffixes
-            .as_ref()
-            .unwrap()
-            .iter()
-            .filter(|suffix| suffix.0 == now)
-            .map(|suffix| suffix.1.unwrap_or(0))
-            .max()
-            .map(|n| n + 1);
-
-        // Register the selected suffix as taken
-        self.suffixes.as_mut().unwrap().push_back((now.clone(), n));
-
-        // Remove old files
-        // Note that the oldest are the first in the list
-        let to_delete = match self.file_limit {
-            FileLimit::MaxFiles(max_files) => {
-                let n_files = self.suffixes.as_ref().unwrap().len();
-                if n_files > max_files {
-                    n_files - max_files
+            let number = if let Some(newest_suffix) = newest_suffix {
+                if newest_suffix.timestamp == now {
+                    Some(newest_suffix.number.unwrap_or(0) + 1)
                 } else {
-                    0
+                    None
                 }
+            } else {
+                None
+            };
+            TimestampSuffix {
+                timestamp: now,
+                number
             }
-            FileLimit::Age(age) => {
-                let mut to_delete = 0;
-                for suffix in self.suffixes.as_ref().unwrap().iter() {
-                    let old_timestamp = (Local::now() - age).format(self.format).to_string();
-                    let delete = suffix.0 < old_timestamp;
-                    if delete {
-                        let _ = std::fs::remove_file(self.suffix_to_path(basepath, suffix));
-                        to_delete += 1;
-                    } else {
-                        // Remember that `suffixes` has the oldest entries in the front, we can `break`
-                        // once we find an entry that doesn't have to deleted
-                        break;
-                    }
-                }
-                to_delete
-            }
-        };
-
-        // Delete respective entries
-        for _ in 0..to_delete {
-            self.suffixes.as_mut().unwrap().pop_front();
+        } else {
+            // This rotation scheme dictates that only the main log file should ever be renamed.
+            // TODO: do something else than panic
+            panic!("programmer error in TimestampSuffixScheme::rotate_file")
         }
-
-        self.suffix_to_string(&(now, n))
     }
-    fn log_paths(&mut self, basepath: &Path) -> Vec<PathBuf> {
-        self.ensure_suffix_list(basepath);
-        self.suffixes
-            .as_ref()
-            .unwrap()
-            .iter()
-            .map(|suffix| {
-                PathBuf::from(format!(
-                    "{}.{}",
-                    basepath.display(),
-                    self.suffix_to_string(suffix)
-                ))
+    fn parse(&self, suffix: &str) -> Option<Self::Repr> {
+        let (timestamp_str, n) = if let Some(dot) = suffix.find('.') {
+            if let Ok(n) = suffix[(dot + 1)..].parse::<usize>() {
+                (&suffix[..dot], Some(n))
+            } else {
+                return None;
+            }
+        } else {
+            (suffix, None)
+        };
+        NaiveDateTime::parse_from_str(timestamp_str, self.format)
+            .map(|_| TimestampSuffix {
+                timestamp: timestamp_str.to_string(),
+                number: n,
             })
-            .collect::<Vec<_>>()
+            .ok()
+    }
+    fn too_old(&self, suffix: &TimestampSuffix, file_number: usize) -> bool {
+        match self.file_limit {
+            FileLimit::MaxFiles(max_files) => file_number >= max_files,
+            FileLimit::Age(age) => {
+                let old_timestamp = (Local::now() - age).format(self.format).to_string();
+                suffix.timestamp < old_timestamp
+            }
+        }
     }
 }
 
-/// How to determine if a file should be deleted, in the case of TimestampSuffix.
+/// How to determine if a file should be deleted, in the case of TimestampSuffixScheme.
 #[cfg(feature = "chrono04")]
 pub enum FileLimit {
     /// Delete the oldest files if number of files is too high
     MaxFiles(usize),
     /// Delete files that have too old timestamp
     Age(Duration),
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn timestamp_ordering() {
+        assert!(
+            TimestampSuffix {
+                timestamp: "2021".to_string(),
+                number: None
+            } < TimestampSuffix {
+                timestamp: "2020".to_string(),
+                number: None
+            }
+        );
+        assert!(
+            TimestampSuffix {
+                timestamp: "2021".to_string(),
+                number: Some(1)
+            } < TimestampSuffix {
+                timestamp: "2021".to_string(),
+                number: None
+            }
+        );
+    }
 }
